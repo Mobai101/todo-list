ca1bd620dbb8ba31fc953fed71e6f3fdb882cc06
tranledong <tranledong0101@gmail.com>
2024-03-15 09:18:44 +0700

    remove font awesome folder and replace with svg

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -4,7 +4,11 @@
   <div class="input">
     <input type="text" class="todo-input" name="todo" id="todo" />
     <button class="todo-button">
-      <i class="fas fa-plus-square"></i>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path
+          d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM200 344V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H248v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"
+        />
+      </svg>
     </button>
   </div>
 </template>
@@ -30,13 +34,16 @@
     width: 2.75rem;
     background-color: var(--white);
     border: none;
-    font-size: 2rem;
-    color: var(--orange);
+    fill: var(--orange);
+  }
+
+  .todo-button svg {
+    width: 1.8rem;
   }
 
   .todo-button:hover {
     background-color: var(--orange);
-    color: var(--white);
+    fill: var(--white);
   }
 
   .todo-button:active {
